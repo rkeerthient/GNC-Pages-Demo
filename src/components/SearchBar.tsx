a8290a4869e10965290c3f3ac2915ebadb380791
Aaron Pavlick <apavlick@yext.com>
2023-02-03 14:18:52 -0700

    vis auto vertical ordering and duplicate removal

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -32,34 +32,45 @@ const SearchBar = () => {
       ?.results as unknown as Result<Ce_product>[] | undefined;
     const articleResults = verticalKeyToResults["articles"]?.results;
     const categoryResults = verticalKeyToResults["categories"]?.results;
+    const orderedKeys = Object.keys(verticalKeyToResults);
 
     return (
       <div className="fixed top-[68px] max-h-[calc(100vh-68px)] overflow-y-scroll py-4 left-0 right-0 bg-stone-100 bottom-0">
-        {/* 44px + py 6 */}
-        {productResults &&
-          productResults.map((result) => (
-            <ProductCard key={result.id} result={result} />
-          ))}
-        {categoryResults && (
-          <div className="p-4">
-            <p className="mb-4 font-bold">CATEGORIES</p>
-            {categoryResults.map((result) => (
-              <p key={result.id} className="mb-4 text-sm">
-                {result.name}
-              </p>
-            ))}
-          </div>
-        )}
-        {articleResults && (
-          <div className="p-4">
-            <p className="mb-4 font-bold">ARTICLES</p>
-            {articleResults.map((result) => (
-              <p key={result.id} className="mb-4 text-sm">
-                {result.name}
-              </p>
-            ))}
-          </div>
-        )}
+        {orderedKeys.map((key) => {
+          if (key === "products" && productResults) {
+            // remove products with the same name
+            return productResults
+              .filter(
+                (product, index, self) =>
+                  index === self.findIndex((p) => p.name === product.name)
+              )
+              .map((result) => <ProductCard key={result.id} result={result} />);
+          } else {
+            if (key === "categories" && categoryResults) {
+              return (
+                <div key="products" className="p-4">
+                  <p className="mb-4 font-bold">CATEGORIES</p>
+                  {categoryResults.map((result) => (
+                    <p key={result.id} className="mb-4 text-sm">
+                      {result.name}
+                    </p>
+                  ))}
+                </div>
+              );
+            } else if (key === "articles" && articleResults) {
+              return (
+                <div key="articles" className="p-4">
+                  <p className="mb-4 font-bold">ARTICLES</p>
+                  {articleResults.map((result) => (
+                    <p key={result.id} className="mb-4 text-sm">
+                      {result.name}
+                    </p>
+                  ))}
+                </div>
+              );
+            }
+          }
+        })}
       </div>
     );
   };
